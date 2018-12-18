37d4959d3a8753580c5186de8a72840a7648dc51
abrockmeier <alexander.brockmeier@ambicotech.com>
2018-12-18 12:53:02 +0100

    v0.1
    pre commit

---
+++ Zahlen_Ausgesprochen_C-sharp/CZahl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahlen_Ausgesprochen_C_sharp
+{
+	class CZahl
+	{
+		int i;
+		//private int eingabe;
+		private readonly string hunderter, einer, zehner;
+
+		public int I { get => i; set => i = value; }
+
+		public CZahl (int i)
+		{
+		//return;
+		}
+		//public int Eingabe { get => eingabe; set => eingabe = value; }
+
+		/*private static void Eingabe()
+		{
+			
+		}
+		*/
+
+
+		private string Spell(int I) // kann auch direkt umwandeln...
+		{
+			
+			return "empty";
+		}
+		/*private void Eingabe(int eingabe)
+		{
+		Convert.ToInt32(Console.ReadLine());
+		}
+		*/
+	}
+}
--- Zahlen_Ausgesprochen_C-sharp/Program.cs
+++ Zahlen_Ausgesprochen_C-sharp/Program.cs
@@ -10,8 +10,16 @@ namespace Zahlen_Ausgesprochen_C_sharp
 	{
 		static void Main(string[] args)
 		{
+			
+			Console.Write("Bitte eine Zahl eingeben:");
+			// Eingabe();
+			int eingabe = Convert.ToInt32(Console.ReadLine());
+			CZahl zahl = new CZahl(eingabe);
+			zahl.I =  eingabe;
 
-
+			// Console.WriteLine(hunderter + "und" + einer + zehner, hunderter, einer, zehner);
 		}
+
+		
 	}
 }

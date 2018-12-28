ff362020c635bdad86b448cb661ab88d9978faa2
abrockmeier <alexander.brockmeier@ambicotech.com>
2018-12-28 03:37:55 +0100

    v.2.1
    almost done ;-)

--- Zahlen_Ausgesprochen_C-sharp/Program.cs
+++ Zahlen_Ausgesprochen_C-sharp/Program.cs
@@ -8,18 +8,129 @@ namespace Zahlen_Ausgesprochen_C_sharp
 {
 	class Program
 	{
+		//MAIN... incl EINGABE und Programmablauf
 		static void Main(string[] args)
 		{
-			
-			Console.Write("Bitte eine Zahl eingeben:");
-			// Eingabe();
-			int eingabe = Convert.ToInt32(Console.ReadLine());
-			CZahl zahl = new CZahl(eingabe);
-			zahl.I =  eingabe;
 
-			// Console.WriteLine(hunderter + "und" + einer + zehner, hunderter, einer, zehner);
+			Console.Write("Bitte eine Zahl eingeben:");
+			string eingabe = Console.ReadLine();
+			int inteingabe;
+			bool ceingabe = Int32.TryParse(eingabe, out (inteingabe));
+
+
+
+			while (!ceingabe) //ToDo: in Methode packen 
+			{
+				Console.WriteLine("Sie haben {0}, eingegeben! Bitte eine Zahl" +
+				" zwischen -2.147.483.648 und 2.147.483.647 eingeben", eingabe);
+				eingabe = Console.ReadLine();
+				ceingabe = Int32.TryParse(eingabe, out (inteingabe));
+			}
+			int stellen = anZahlen(inteingabe);
+
+
+			// Console.WriteLine(hunderter + "und" + einer + zehner, hunderter
+			// , einer, zehner);
 		}
 
+		//VERARBEITUNG
+
+		static string ConvertToAusgesprochen(int Zahl, int stellen)
+		{
+			if (Zahl == 0)
+			{
+				Console.WriteLine("Null ( nicht NULL! )");
+				return ("Null !NULL");
+
+			}
+			else if (stellen == 1)
+			{
+				// pos INT
+				return("1-9");
+				
+			}
+			else if (stellen == 2)
+			{
+				// pos INT
+				return("10-99");
+				
+			}
+			else if (stellen == 3)
+			{
+				// pos INT
+				return("100-999");
+				
+			}
+			else if (stellen == 4)
+			{
+				// pos INT
+				return("1000-9999");
+				
+			}
+			else if (Zahl <= 10000) // stellen <= 2 &&
+			{
+				// pos INT
+				return("Sie sind größenwahsinnig! Ihre Zahl hat: "+stellen + " Stellen...");
+				
+			}
+			
+			else
+			{
+				
+				return("else fall");
+			}
+		}
 		
+
+		static int anZahlen(int n)
+		{
+			if (n < 0)
+			{
+			n = (n == Int32.MinValue) ? Int32.MaxValue : -n;
+			}
+			if (n < 10) return 1;
+			if (n < 100) return 2;
+			if (n < 1000) return 3;
+			if (n < 10000) return 4;
+			if (n < 100000) return 5;
+			if (n < 1000000) return 6;
+			if (n < 10000000) return 7;
+			if (n < 100000000) return 8;
+			if (n < 1000000000) return 9;
+			else return 10;
+		}
+
+		static string einerStelle(int einer)
+		{
+			return ("einer");
+		// Einerstelle bestimmen und String zurückgeben.
+		}
+
+		static string zehnerStelle(int zehner)
+		{
+			return ("Zehner");
+		// Einerstelle bestimmen und String zurückgeben.
+		}
+
+		static string hunderterStelle()
+		{
+			return ("hunderter");
+		}
+
+		static string tausenderStellen()
+		{
+			return ("tausender");
+		}
+		
+		// AUSGABE
+
+
+		static void Ausgabe()
+		{
+
+
+		}
 	}
 }
+
+
